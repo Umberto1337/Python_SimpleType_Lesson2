3dff00119bd1f44706d6c7e74286b878bc759a6e
Alexey <cappuccino13371@gmail.com>
2024-02-05 16:59:07 +0300

    update math_1

---
+++ task_math_1.py
@@ -0,0 +1,47 @@
+'''Математические функции'''
+# import math
+
+# print(math.pi, math.e, math.inf, math.nan, math.tau, sep='\n')
+
+# print(dir(math))
+
+'''Модуль decimal'''
+# import decimal
+
+# print(0.1 + 0.2)
+
+# pi = decimal.Decimal('3.141_592_653_589_793_238_462_643_383_279_502_884_197_169_399_375')
+# print(pi)
+# num = decimal.Decimal(1) / decimal.Decimal(3)
+# print(num)
+
+# decimal.getcontext().prec = 120
+# science = 2 * pi * decimal.Decimal(23.452346) ** 2
+# print(science)
+
+'''Модуль franctions'''
+# import fractions
+
+# f1 = fractions.Fraction(1, 3)
+# print(f1)
+# f2 = fractions.Fraction(3, 5)
+# print(f2)
+# print(f1 * f2)
+
+'''Комплексные числа'''
+# a = complex(2, 3)
+# b = complex('2+3j')
+# print(a, b, a == b, sep='\n')
+
+'''Математические Функции из коробки'''
+# x = -42
+# print(abs(x))
+
+# a = 42
+# b = 5
+# print(divmod(a, b))
+
+# print(pow(a, b))
+# print(pow(a, b, 10))
+
+# print(round(3.141_592_653_589_793, 3))
