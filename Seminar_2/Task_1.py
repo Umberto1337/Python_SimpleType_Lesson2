d9f43f3545a25cb07a632652364034ca9929e320
Alexey <cappuccino13371@gmail.com>
2024-02-07 20:51:17 +0300

    update task_2 and task_3

--- Seminar_2/Task_1.py
+++ Seminar_2/Task_1.py
@@ -1,11 +1,11 @@
-import sys
+# import sys
 
-data = [42, 73.0, 'Hello World', True, 42, 'Hello World!', 256, 2 ** 8, 1, 'Привет, мир!']
-for n, item in enumerate(data, 1):
-    # print(n, item)
-    check_int = 'Число явялется целым' if isinstance(item, int) else ''
-    check_str = 'Это строка' if isinstance(item, str) else ''
-    check_float = 'Это вещественное число' if isinstance(item, float) else ''
-    print(f'{n}. Объект {item}\nАдрес: {id(item)}\tРазмер: {sys.getsizeof(item)}\t'
-          f'Хэш: {hash(item)} {check_int}{check_str}{check_float}')
+# data = [42, 73.0, 'Hello World', True, 42, 'Hello World!', 256, 2 ** 8, 1, 'Привет, мир!']
+# for n, item in enumerate(data, 1):
+#     # print(n, item)
+#     check_int = 'Число явялется целым' if isinstance(item, int) else ''
+#     check_str = 'Это строка' if isinstance(item, str) else ''
+#     check_float = 'Это вещественное число' if isinstance(item, float) else ''
+#     print(f'{n}. Объект {item}\nАдрес: {id(item)}\tРазмер: {sys.getsizeof(item)}\t'
+#           f'Хэш: {hash(item)} {check_int}{check_str}{check_float}')
     
---
+++ Seminar_2/task_2.py
@@ -0,0 +1,47 @@
+# Задание №3
+# ✔ Напишите программу, которая получает целое число и возвращает
+# его двоичное, восьмеричное строковое представление.
+# ✔ Функции bin и oct используйте для проверки своего
+# результата, а не для решения.
+# Дополнительно:
+# ✔ Попробуйте избежать дублирования кода
+# в преобразованиях к разным системам счисления
+# ✔ Избегайте магических чисел
+# ✔ Добавьте аннотацию типов где это возможно
+
+# BIN = 2
+# OCT = 8
+
+# num: int = int(input('Введите число: '))
+
+# test_num: int = num
+# result: str = ''
+# res: str = ''
+
+# while test_num:
+#     cur_num = test_num % BIN
+#     result = str(cur_num) + result
+#     test_num //= BIN
+    
+#     cur_num2 = test_num % OCT
+#     res = str(cur_num2) + res
+#     test_num //= OCT
+
+# print(f'для {BIN} {result=}')
+# print(f'для {OCT} {res=}')
+
+# BIN = 2
+# OCT = 8
+
+# num: int = int(input('Введите число: '))
+
+# for div in BIN, OCT:
+#     test_num: int = num
+#     result: str = ''
+#     res: str = ''
+
+#     while test_num:
+#         cur_num = test_num % div
+#         result = str(cur_num) + result
+#         test_num //= div
+#     print(f'для {div} {result=}')
